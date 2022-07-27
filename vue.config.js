47d2b186112b2edbec364ed9394774c82ed52ac2
qiangmouren <2962051004@qq.com>
2022-07-27 12:46:14 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   productionSourceMap: false,
   publicPath: process.env.NODE_ENV === 'production' ? '/joke-recaptcha/' : '',
+  chainWebpack: (config) => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap((options) => {
+        options.limit = Infinity;
+        return options;
+      });
+  },
 };
